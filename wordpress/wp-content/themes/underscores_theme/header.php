4ac6c97636da12e04e2c7f72c489e0dc7ae012da
Torky Almohaisen <torky.almohaisen@hotmail.com>
2014-12-27 09:01:25 +0300

    Place nav in its correct position. Work on finishing up the banner next.

--- wordpress/wp-content/themes/underscores_theme/header.php
+++ wordpress/wp-content/themes/underscores_theme/header.php
@@ -23,14 +23,14 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
-			<img src="<?php bloginfo('template_url'); ?>/media/profpic.jpeg" alt="">
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<img src="<?php bloginfo('template_url'); ?>/media/profpic.jpeg" alt="">
 		</div><!-- .site-branding -->
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'underscores_theme' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
